f2ab54bd161648718b33683f4e08d9ec9d848ba2
Vishu Sidana <vishusidana95@gmail.com>
2020-03-12 00:54:01 +0530

    bitbnsController updated with getRates() function

--- bitbnsController.py
+++ bitbnsController.py
@@ -9,12 +9,30 @@ class bitbnsController():
         payload = {}
         headers = {}
         response = requests.request("GET", url, headers=headers, data = payload)
-        return response
+        try:
+            if(response.status_code == 200):
+                return response
+            else:
+                return None
+        except Exception as e:
+            raise e
+        return None
 
     def getJSONData(self):
         getRawRates = self.getAllTickers()
+        if(getRawRates == None): # Add last 10 sec old request
+            return None
         getJsonRates = getRawRates.json()
         return getJsonRates
 
-    def getBuyRates(self,currentList):
+    def getRates(self,currentList):
+        lastRates = self.getJSONData()
+        returnDictionary = {}
+        for i in currentList: # Apply try catch if currency is not present
+            currentInfo = lastRates[i]
+            buySellList = []
+            buySellList.append(currentInfo['highest_buy_bid'])
+            buySellList.append(currentInfo['lowest_sell_bid'])
+            returnDictionary[i] = buySellList
+        return returnDictionary
         
